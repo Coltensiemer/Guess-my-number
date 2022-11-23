85a94eba255ad98fd82da2a7746d735d3d1d5a09
Coltensiemer <colten50@hotmail.com>
2022-11-23 18:32:23 -0500

    update

--- js.js
+++ js.js
@@ -5,11 +5,11 @@ const again = document.getElementById("again-btn"); // restart the page beside h
 const numberMain = document.getElementById('the-number'); // random number/ ? 
 const numberInput = document.querySelector('.guess'); // input 
 const numberBtn = document.getElementById('input-btn'); //check button 
-const scoreNumber = document.querySelector('.lives'); //lives value 
+const livesLeft = document.querySelector('.lives'); //lives value 
 const highScore = document.querySelector('.highscore'); // high lives 
 const message = document.querySelector('.message'); // message 
 const nextLevelBtn = document.getElementById('next-level'); // next level btn 
-const header = document.querySelector('.header'); // Gues the number 
+const header = document.querySelector('.header'); // Guess the number 
 const currentScore = document.querySelector(".score-number");/// the score number 
 
 
@@ -82,20 +82,13 @@ numberBtn.addEventListener('click', () => {
 
          
          
-         else if ( lives > 3) {
+         else if ( lives > 1) {
          displayMessage(numberInput.value > randomNumberLvl1 ? "Too high": "Too Low") 
              lives--; 
-             scoreNumber.textContent = lives; } 
+             livesLeft.textContent = lives; } 
 
-            //  FIX
-             else if (lives >1 && lives <=3) { 
-                displayMessage(numberInput.value > randomNumberLvl1 ? "Too high": "Too Low") 
-             lives--; 
-             scoreNumber.textContent = lives; 
-             scoreNumber.classList.add("lives-left"); 
-             }
              else { 
-                 scoreNumber.textContent = '0'
+                 livesLeft.textContent = '0'
                  displayMessage("Game-Over")
                  again.style.background = "red"
                  again.style.color = "white"
@@ -130,9 +123,15 @@ header.innerHTML = `Guess the Number (1 to ${levelToGuess})`;
 document.body.style.backgroundColor = '#FF5733'; 
 numberInput.value = ''; 
 numberMain.textContent = 'Take a guess'
-scoreNumber.textContent = lives;   
+ 
 randomNumberLvl1 = (Math.trunc(Math.random() * addFive) + 1); 
-numberMain.classList.remove("scale");  } 
+numberMain.classList.remove("scale");  
+
+livesLeft.textContent = lives;  
+document.querySelector('.addThree').classList.add("addThreeAd"); 
+} 
+
+
 
 }
 ) 
@@ -156,11 +155,14 @@ numberMain.textContent = 'Guess a number'
 again.style.background = ""
 again.style.color = "black"
 
+document.querySelector('.level').textContent = `Level ${level}`; 
 currentScore.textContent = currentValue; 
-scoreNumber.textContent = lives; 
+livesLeft.textContent = lives; 
 
 
 } 
 )
 
-
+if(lives >1 && lives < 3){ 
+     livesLeft.classList.add("lives-left"); 
+     }
